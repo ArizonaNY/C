a82a6ba4d89561eb0c6e6ba48708b94db095c07c
ArizonaNY <125306670+ArizonaNY@users.noreply.github.com>
2023-08-23 13:34:47 -0300

    Create Multiplicação.C
    Fazer contas de Multiplicação

---
+++ Multiplicação.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, X;
+    printf("Digite o valor de a e b: \n");
+    scanf("%d %d", &a, &b);
+    X = a + b;
+    printf("X = %d\n", X);
+    return 0;
+}
